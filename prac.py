7005801fcb93c478eb98aec5345a8f6203f56925
Sean OShea <sosean466@gmail.com>
2020-07-10 14:07:21 -0500

    Initial commit

---
+++ prac.py
@@ -0,0 +1,12 @@
+
+
+Truck_01 = 55225
+Truck_02 = 60005
+if Truck_01 > 52000:
+    print("Truck 01...service due")
+else:
+    print("Truck 01...In service")
+if Truck_02 > 60500:
+    print("Truck 02...service due")
+else:
+    print("Truck 02...In service")
